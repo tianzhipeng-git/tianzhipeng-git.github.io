c57ad5b8be3546082a4e4ed7438064b3c59ee4f3
tzp <847379570@qq.com>
2019-07-13 13:46:08 +0800

    放弃adoc, 改大字体

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,18 @@
 $(function () {
+    String.prototype.hashCode = function () {
+        for (var ret = 0, i = 0, len = this.length; i < len; i++) {
+            ret = (31 * ret + this.charCodeAt(i)) << 0;
+        }
+        return ret;
+    };
+    var _hmt = _hmt || [];
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?5c3649ca110d768aa09517fcece1a8d3";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+
     $("#gotop").on('click', function () {
         jQuery("html,body").animate({
             scrollTop: 0
